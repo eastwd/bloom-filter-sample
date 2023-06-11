9672d23e77f477a1068c52ea5b4f407128d8e624
eastwd <bando142141@gmail.com>
2023-06-11 15:19:01 +0900

    fix

--- main.go
+++ main.go
@@ -10,15 +10,11 @@ const (
     hashFunctionAmount = 3
     hashLength = 18
     byteMax = 255
-    m = hashLength
-    k = hashFunctionAmount
 )
 
 var (
     filter = [hashLength]bool{}
 )
-
-type hashFunction func(data string)[hashLength]string
 
 var (
     dataSet = []string{
@@ -28,10 +24,7 @@ var (
     }
 )
 
-// 追加された新しい要素 input
-// ハッシュテーブルの位置
 //byte型は0~255までをとる
-//ハッシュ関数の数の分だけ先頭から値を取得し、ハッシュ配列の長さでbyte型の返却値を正規化した時の値を配列で返す
 func UpdateFilter(input string) {
     h := sha512.Sum512([]byte(input))
     for i:=0; i< hashFunctionAmount; i++ {
